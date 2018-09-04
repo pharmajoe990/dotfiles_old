5411ae5920fd0bf810ab509df498b5b4ae50c2d9
Tim Roper <tim.roper@myob.com>
2018-09-05 09:37:18 +1000

    Simple script to create links for dotfiles

---
+++ link_files.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'FileUtils'
+
+
+# unless specified, no path means target dir is $HOME
+dotFileLocations = { 
+  'aliases' => '.aliases',
+  'gitconfig' => '.gitconfig',
+  'nvimrc' => '.nvimrc',	# Need to source this in ~/.config/nvim/init.vim
+  'zshenv' =>  '.zshenv',
+  'zshrc' => '.zshrc'
+}
+
+homeDir = File.expand_path('~') + File::SEPARATOR
+dotFilePath = homeDir + '.dotfiles' + File::SEPARATOR
+dotFileLocations.each { |file, targetLocation|
+  begin
+    FileUtils.ln_s dotFilePath + file, homeDir + targetLocation 
+  rescue Errno::EEXIST
+    puts "#{homeDir + targetLocation} already exists, skipping"
+  else
+    puts "#{homeDir + targetLocation} created"
+  end
+}
+
+
